3c5682b22f28d98b8ec4c1e6385fc5ab5bbe1637
邹平 <zouping0428@163.com>
2026-05-30 08:49:32 +0800

    1

--- src/Zpulon.AICopilot.MigrationWorkApp/SeedData/McpServerInfoData.cs
+++ src/Zpulon.AICopilot.MigrationWorkApp/SeedData/McpServerInfoData.cs
@@ -21,7 +21,7 @@ public static class McpServerInfoData
             "提供本地文件系统访问能力，它允许在限定的目录范围内执行文件和目录相关操作，包括读取文件内容、创建和写入文件、列出目录结构、移动或重命名文件等。",
             McpTransportType.Stdio,
             "npx",
-            @"-y @modelcontextprotocol/server-filesystem E:\Test",
+            @"-y @modelcontextprotocol/server-filesystem C:\Test",
             fileSystemRisks
         );  
         
